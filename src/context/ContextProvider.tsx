5c86e7a95eaae919e3f191f6fb938917ad80ef66
ConCapbreton <connorsexton@hotmail.com>
2024-09-13 18:30:28 +0200

    ChangeErrorMessage

--- src/context/ContextProvider.tsx
+++ src/context/ContextProvider.tsx
@@ -121,7 +121,7 @@ const ContextProvider = ({children}: ChildrenType) => {
         const getStoredApiData = JSON.parse(localStorage.getItem("storedApiData") as string)
         const getStoredTideData = JSON.parse(localStorage.getItem("storedTideData") as string)
         const getStoredDayLengthData = JSON.parse(localStorage.getItem("storedDayLengthData") as string)
-        if (getStoredApiData.errors) {setErrorMsg(["Clicking the Reload button should hopefully fix this error!"]); setIsLoading(false);}      
+        if (getStoredApiData.errors) {setErrorMsg(["Error!"]); setIsLoading(false);}      
         let apiDate = getStoredApiData.meta.start
         let cutApiDate = apiDate.slice(0, 10)
         if (formattedDateString === cutApiDate) {
